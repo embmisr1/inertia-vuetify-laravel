c289f5480feda1dba7b9a5be63167eae2db71fb8
allandaryldev <allandaryl.dev@gmail.com>
2022-06-08 10:19:47 +0800

    sidebar patch

--- resources/js/src/layouts/default.vue
+++ resources/js/src/layouts/default.vue
@@ -286,22 +286,22 @@ export default {
                             child: [
                                 {
                                     name: "PROJECT TYPE",
-                                    link: "",
+                                    link: "/app/project/type",
                                     icon: "mdi-link",
                                 },
                                 {
                                     name: "PROJECT SUB-TYPE",
-                                    link: "",
+                                    link: "/app/project/subtype",
                                     icon: "mdi-link",
                                 },
                                 {
                                     name: "PROJECT SPECIFIC TYPE",
-                                    link: "",
+                                    link: "/app/project/specifictype",
                                     icon: "mdi-link",
                                 },
                                 {
                                     name: "PROJECT SPECIFIC SUB-TYPE",
-                                    link: "",
+                                    link: "/app/project/specificsubtype",
                                     icon: "mdi-link",
                                 },
                                 {
