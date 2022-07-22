87b4334f97749f5ddbae0f35905a14fb8e2e1ca0
russelowens <russelowens.miranda@lorma.edu>
2022-07-22 13:50:23 +0800

    07222022_0150

--- app/Http/Controllers/SolidwasteLCEController.php
+++ app/Http/Controllers/SolidwasteLCEController.php
@@ -3,12 +3,75 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Inertia\Inertia;
+use App\Models\SWM\LCE;
 use App\Models\SolidwasteLCE;
 use App\Models\Barangay;
 use App\Models\Municipality;
 use App\Models\Province;
+use Illuminate\Support\Facades\DB;
 
 class SolidwasteLCEController extends Controller
 {
-    //
+    public function lce_testing_route(){
+        //
+    }
+    public function rce_list(request $request){
+        $lce_list = DB::table('tbl_solidwaste_lce as a')
+        ->select('a.*', 'b.provDesc', 'c.citymunDesc', 'd.brgyDesc')
+        ->leftjoin('ref_province as b', 'a.lce_province_FK', '=', 'b.PK_province_ID')
+        ->leftjoin('ref_citymun as c', 'a.lce_municipality_FK', '=', 'c.PK_citymun_ID')
+        ->leftjoin('ref_brgy as d', 'a.lce_barangay_FK', '=', 'd.PK_brgy_ID')
+        ->when(request('filter_province'), function ($lce_list) {
+            $lce_list->where('a.lce_province_FK', request('filter_province'));
+        })
+        ->when(request('filter_municipality'), function ($lce_list) {
+            $lce_list->where('a.lce_municipality_FK', request('filter_municipality'));
+        })
+        ->when(request('filter_barangay'), function ($lce_list) {
+            $lce_list->where('a.lce_barangay_FK', request('filter_barangay'));
+        })
+        ->paginate(10);
+        return Inertia::render("pages/solidwaste/lce",[ 
+            'lce_list'=>$lce_list
+        ]);
+    }
+    public function rce_register_process(request $request){
+        $query = new SolidwasteLCE();
+        $query->lce_title = $request->lce_title;
+        $query->lce_first_name = $request->lce_first_name;
+        $query->lce_middle_name = $request->lce_middle_name;
+        $query->lce_last_name = $request->lce_last_name;
+        $query->lce_salutation = $request->lce_salutation;
+        $query->lce_position = $request->lce_position;
+        $query->lce_province_FK = $request->lce_province_FK;
+        $query->lce_municipality_FK = $request->lce_municipality_FK;
+        $query->lce_barangay_FK = $request->lce_barangay_FK;
+        $query->lce_disctrict = $request->lce_disctrict;
+        $query->lce_zip_code = $request->lce_zip_code;
+        $query->lce_focal_person = $request->lce_focal_person;
+        $query->lce_contact_number = $request->lce_contact_number;
+        $query->lce_email_address = $request->lce_email_address;
+        $query->save();
+        return back();
+    }
+    public function rce_update_process(request $request){
+        $query = SolidwasteLCE::find($request->id);
+        $query->lce_title = $request->lce_title;
+        $query->lce_first_name = $request->lce_first_name;
+        $query->lce_middle_name = $request->lce_middle_name;
+        $query->lce_last_name = $request->lce_last_name;
+        $query->lce_salutation = $request->lce_salutation;
+        $query->lce_position = $request->lce_position;
+        $query->lce_province_FK = $request->lce_province_FK;
+        $query->lce_municipality_FK = $request->lce_municipality_FK;
+        $query->lce_barangay_FK = $request->lce_barangay_FK;
+        $query->lce_disctrict = $request->lce_disctrict;
+        $query->lce_zip_code = $request->lce_zip_code;
+        $query->lce_focal_person = $request->lce_focal_person;
+        $query->lce_contact_number = $request->lce_contact_number;
+        $query->lce_email_address = $request->lce_email_address;
+        $query->save();
+        return $request->id;
+    }
 }
--- app/Models/SWM/LCE.php
+++ app/Models/SWM/LCE.php
@@ -4,6 +4,7 @@ namespace App\Models\SWM;
 
 use App\Models\Barangay;
 use App\Models\Municipality;
+use App\Models\Province;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -12,49 +13,50 @@ class LCE extends Model
     use HasFactory;
     protected $table = 'tbl_solidwaste_lce';
     protected $fillable = [
-        "lca_title",
-        "lca_first_name",
-        "lca_middle_name",
-        "lca_last_name",
-        "lca_salutation",
-        "lca_position",
-        "lca_municipality_FK",
-        "lca_province_FK",
+        "lce_title",
+        "lce_first_name",
+        "lce_middle_name",
+        "lce_last_name",
+        "lce_salutation",
+        "lce_position",
+        "lce_municipality_FK",
+        "lce_province_FK",
         "lce_barangay_FK",
-        "lca_disctrict",
-        "lca_zip_code",
-        "lca_focal_person",
-        "lca_contact_number",
-        "lca_email_address",
+        "lce_disctrict",
+        "lce_zip_code",
+        "lce_focal_person",
+        "lce_contact_number",
+        "lce_email_address",
     ];
-    public function lca_province()
+    
+    public function lce_province()
     {
-        return $this->belongsTo(Province::class, "lca_province_FK");
+        return $this->belongsTo(Province::class, "lce_province_FK", "PK_province_ID");
     }
-    public function lca_municipality()
+    public function lce_municipality()
     {
-        return $this->belongsTo(Municipality::class, "lca_municipality_FK");
+        return $this->belongsTo(Municipality::class, "lce_municipality_FK");
     }
     public function lce_barangay()
     {
         return $this->belongsTo(Barangay::class, "lce_barangay_FK");
     }
-    public function setlca_complete_address($value)
+    public function setlce_complete_address($value)
     {
-        return $this->lca_province.', '. $this->lca_municipality. ', ' . $this->lce_barangay. ', ' . $this->lca_disctrict. ', ' . $this->lca_zip_code;
+        return $this->lce_province.', '. $this->lce_municipality. ', ' . $this->lce_barangay. ', ' . $this->lce_disctrict. ', ' . $this->lce_zip_code;
     }
 
     public function setnamewithtitle($value)
     {
-        return $this->title . ' ' .$this->lca_first_name. ' ' .$this->lca_middle_name. ' ' .$this->lca_last_name;
+        return $this->title . ' ' .$this->lce_first_name. ' ' .$this->lce_middle_name. ' ' .$this->lce_last_name;
     }
 
     public function setnamewithsalutation($value)
     {
-        return $this->lca_salutation . ' ' .$this->lca_first_name. ' ' .$this->lca_middle_name. ' ' .$this->lca_last_name;
+        return $this->lce_salutation . ' ' .$this->lce_first_name. ' ' .$this->lce_middle_name. ' ' .$this->lce_last_name;
     }
     public function setdecentname($value)
     {
-        return $this->lca_salutation . ' ' .$this->lca_first_name. ' ' .$this->lca_middle_name. ' ' .$this->lca_last_name;
+        return $this->lce_salutation . ' ' .$this->lce_first_name. ' ' .$this->lce_middle_name. ' ' .$this->lce_last_name;
     }
 }
