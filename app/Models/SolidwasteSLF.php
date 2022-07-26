7b84cefe09c54cb889ca01e7ff7796113ccdece8
allandaryldev <allandaryl.dev@gmail.com>
2022-07-26 14:31:53 +0800

    added media-library

--- app/Models/SolidwasteSLF.php
+++ app/Models/SolidwasteSLF.php
@@ -12,4 +12,42 @@ class SolidwasteSLF extends Model implements HasMedia
 {
     protected $table = 'tbl_solidwaste_slf';
     use HasFactory, InteractsWithMedia;
+
+    protected $fillable = [
+        "slf_complete_address",
+        "slf_latitude",
+        "slf_longitude",
+        "slf_project_operator",
+        "slf_ecc_number",
+        "slf_contact_person",
+        "slf_contact_number",
+        "slf_category",
+        "slf_site_hectares",
+        "slf_total_capacity",
+        "slf_tons_per_day",
+        "slf_service_life",
+        "slf_remaining_service_life",
+        "slf_exceeded_capacity",
+        "slf_with_planned_extension",
+        "slf_lgu_served",
+        "slf_leachate_treatment",
+        "slf_daily_soil_cover",
+        "slf_presence_of_mrf",
+        "slf_separate_cells_for_hazwaste",
+        "slf_methane_recovery",
+        "slf_discharge_permit",
+        "slf_1586_compliance",
+        "slf_9275_compliance",
+        "slf_6969_compliance",
+        // "slf_file",
+        "lce_FK",
+    ];
+
+    public function registerMediaCollections(): void
+    {
+        $this
+            ->addMediaCollection('slf')
+            ->acceptsMimeTypes(['image/jpeg', 'image/png', 'image/jpg', 'application/pdf'])
+            ->useDisk('slf');
+    }
 }
