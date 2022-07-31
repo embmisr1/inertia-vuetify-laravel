cd628d41d3db6bb4193e0f5f392420a1a4ab5ad1
allandaryldev <allandaryl.dev@gmail.com>
2022-07-31 09:12:25 +0800

    my error 10_yr_mon_create_form

--- app/Http/Controllers/SolidwasteTenYearController.php
+++ app/Http/Controllers/SolidwasteTenYearController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\AttachmentResource;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Illuminate\Support\Facades\DB;
@@ -11,66 +12,158 @@ use App\Models\SolidwasteTenYearMonitoring;
 class SolidwasteTenYearController extends Controller
 {
 
-    public function create(request $request){
+    public function create(request $request)
+    {
         $id = $request->id;
         $lce_info = DB::table('tbl_solidwaste_lce as a')
-        ->select('a.*', 'b.provDesc', 'c.citymunDesc', 'd.brgyDesc', 'c.districtCode')
-        ->leftjoin('ref_province as b','a.lce_province_FK','=','b.PK_province_ID')
-        ->leftjoin('ref_citymun as c','a.lce_municipality_FK','=','c.PK_citymun_ID')
-        ->leftjoin('ref_brgy as d','a.lce_barangay_FK','=','d.PK_brgy_ID')
-        ->where('id',$id)
-        ->get();
-        return Inertia::render("pages/swm/Form/TenYearForm",[
-            'lce_info'=>$lce_info,
+            ->select('a.*', 'b.provDesc', 'c.citymunDesc', 'd.brgyDesc', 'c.districtCode')
+            ->leftjoin('ref_province as b', 'a.lce_province_FK', '=', 'b.PK_province_ID')
+            ->leftjoin('ref_citymun as c', 'a.lce_municipality_FK', '=', 'c.PK_citymun_ID')
+            ->leftjoin('ref_brgy as d', 'a.lce_barangay_FK', '=', 'd.PK_brgy_ID')
+            ->where('id', $id)
+            ->get();
+        return Inertia::render("pages/swm/Form/TenYrForm", [
+            'lce_info' => $lce_info,
         ]);
     }
 
-    public function ten_year_edit(request $request){
+    public function ten_year_edit(request $request)
+    {
         $id = $request->id;
         $ten_year_edit = DB::table('tbl_solidwaste_ten_year as a')
-        ->select('a.*', 'c.provDesc', 'd.citymunDesc', 'e.brgyDesc', 'd.districtCode')
-        ->leftjoin('tbl_solidwaste_lce as b','a.lce_FK','=','b.id')
-        ->leftjoin('ref_province as c','b.lce_province_FK','=','c.PK_province_ID')
-        ->leftjoin('ref_citymun as d','b.lce_municipality_FK','=','d.PK_citymun_ID')
-        ->leftjoin('ref_brgy as e','b.lce_barangay_FK','=','e.PK_brgy_ID')
-        ->where('a.id',$id)->get();
-        $ten_year_monitoring_list = SolidwasteTenYearMonitoring::where('ten_year_FK',$id)->get();
-        return Inertia::render("pages/swm/Form/TenYearForm",[
-            'ten_year_edit'=>$ten_year_edit,
-            'ten_year_monitoring_list'=>$ten_year_monitoring_list,
+            ->select("a.id","a.lce_FK",'a.ten_year_planning_period', 'a.ten_year_year_approved', 'a.ten_year_number', 'c.provDesc', 'd.citymunDesc', 'e.brgyDesc', 'd.districtCode')
+            // ->select('a.*', 'c.provDesc', 'd.citymunDesc', 'e.brgyDesc', 'd.districtCode')
+            ->leftjoin('tbl_solidwaste_lce as b', 'a.lce_FK', '=', 'b.id')
+            ->leftjoin('ref_province as c', 'b.lce_province_FK', '=', 'c.PK_province_ID')
+            ->leftjoin('ref_citymun as d', 'b.lce_municipality_FK', '=', 'd.PK_citymun_ID')
+            ->leftjoin('ref_brgy as e', 'b.lce_barangay_FK', '=', 'e.PK_brgy_ID')
+            ->where('a.id', $id)->get();
+        $ten_year_monitoring_list = SolidwasteTenYearMonitoring::where('ten_year_FK', $id)->get();
+        $attachements = SolidwasteTenYear::where("id", $id)->get();
+        return Inertia::render("pages/swm/Form/TenYrForm", [
+            'ten_year_edit' => $ten_year_edit,
+            'ten_year_monitoring_list' => $ten_year_monitoring_list,
+            "attachments" => [
+                "ten_yr_file" => AttachmentResource::collection($attachements[0]->getMedia("ten_yr_file")),
+                "copy_plan" => AttachmentResource::collection($attachements[0]->getMedia("copy_plan")),
+                "copy_form" => AttachmentResource::collection($attachements[0]->getMedia("copy_form")),
+                "copy_resolution" => AttachmentResource::collection($attachements[0]->getMedia("copy_resolution")),
+            ]
         ]);
     }
 
-    public function ten_year_register_process(request $request){
-        $query = new SolidwasteTenYear();
-        $query->ten_year_planning_period = $request->ten_year_planning_period;
-        $query->ten_year_year_approved = $request->ten_year_year_approved;
-        $query->ten_year_number = $request->ten_year_number;
-        $query->ten_year_file = $request->ten_year_file;
-        $query->ten_year_copy_plan = $request->ten_year_copy_plan;
-        $query->ten_year_copy_resolution = $request->ten_year_copy_resolution;
-        $query->ten_year_copy_form = $request->ten_year_copy_form;
-        $query->lce_FK = $request->lce_FK;
-        $query->save();
-        return back()->with("message","10 Year Created");
+    public function ten_year_register_process(request $request)
+    {
+        try {
+            $query = new SolidwasteTenYear();
+            $query->ten_year_planning_period = $request->ten_year_planning_period;
+            $query->ten_year_year_approved = $request->ten_year_year_approved;
+            $query->ten_year_number = $request->ten_year_number;
+            $query->ten_year_file = !empty($request->ten_year_file) ? true : false;
+            $query->ten_year_copy_plan = !empty($request->ten_year_copy_plan) ? true : false;
+            $query->ten_year_copy_resolution = !empty($request->ten_year_copy_resolution) ? true : false;
+            $query->ten_year_copy_form = !empty($request->ten_year_copy_form) ? true : false;
+            $query->lce_FK = $request->lce_FK;
+            $query->save();
+
+            if ($request->ten_year_file) {
+                foreach ($request->ten_year_file as $file) {
+                    $query
+                        ->addMedia($file)
+                        ->preservingOriginal()
+                        ->toMediaCollection("ten_yr_file", "ten_yr_file");
+                }
+            }
+
+            if ($request->ten_year_copy_plan) {
+                foreach ($request->ten_year_copy_plan as $file) {
+                    $query
+                        ->addMedia($file)
+                        ->preservingOriginal()
+                        ->toMediaCollection("copy_plan", "copy_plan");
+                }
+            }
+
+            if ($request->ten_year_copy_form) {
+                foreach ($request->ten_year_copy_form as $file) {
+                    $query
+                        ->addMedia($file)
+                        ->preservingOriginal()
+                        ->toMediaCollection("copy_form", "copy_form");
+                }
+            }
+
+            if ($request->ten_year_copy_resolution) {
+                foreach ($request->ten_year_copy_resolution as $file) {
+                    $query
+                        ->addMedia($file)
+                        ->preservingOriginal()
+                        ->toMediaCollection("copy_resolution", "copy_resolution");
+                }
+            }
+            return redirect()->route("lce_show",["id"=>$request->lce_FK])->with("message", "10 Year Created");
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
     }
 
-    public function ten_year_update_process(request $request){
-        $query = SolidwasteTenYear::find($request->id);
+    public function ten_year_update_process(request $request)
+    {
+        try {
+            $query = SolidwasteTenYear::find($request->id);
         $query->ten_year_planning_period = $request->ten_year_planning_period;
         $query->ten_year_year_approved = $request->ten_year_year_approved;
         $query->ten_year_number = $request->ten_year_number;
-        $query->ten_year_file = $request->ten_year_file;
-        $query->ten_year_copy_plan = $request->ten_year_copy_plan;
-        $query->ten_year_copy_resolution = $request->ten_year_copy_resolution;
-        $query->ten_year_copy_form = $request->ten_year_copy_form;
+        // $query->ten_year_file = $request->ten_year_file;
+        // $query->ten_year_copy_plan = $request->ten_year_copy_plan;
+        // $query->ten_year_copy_resolution = $request->ten_year_copy_resolution;
+        // $query->ten_year_copy_form = $request->ten_year_copy_form;
         $query->lce_FK = $request->lce_FK;
         $query->save();
-        return back()->with("message","10 Year Updated");
+        if ($request->ten_year_file) {
+            foreach ($request->ten_year_file as $file) {
+                $query
+                    ->addMedia($file)
+                    ->preservingOriginal()
+                    ->toMediaCollection("ten_yr_file", "ten_yr_file");
+            }
+        }
+
+        if ($request->ten_year_copy_plan) {
+            foreach ($request->ten_year_copy_plan as $file) {
+                $query
+                    ->addMedia($file)
+                    ->preservingOriginal()
+                    ->toMediaCollection("copy_plan", "copy_plan");
+            }
+        }
+
+        if ($request->ten_year_copy_form) {
+            foreach ($request->ten_year_copy_form as $file) {
+                $query
+                    ->addMedia($file)
+                    ->preservingOriginal()
+                    ->toMediaCollection("copy_form", "copy_form");
+            }
+        }
+
+        if ($request->ten_year_copy_resolution) {
+            foreach ($request->ten_year_copy_resolution as $file) {
+                $query
+                    ->addMedia($file)
+                    ->preservingOriginal()
+                    ->toMediaCollection("copy_resolution", "copy_resolution");
+            }
+        }
+        return redirect()->route("lce_show",["id"=>$request->lce_FK])->with("message", "10 Year Updated");
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
     }
-    public function ten_year_delete(request $request){
+    public function ten_year_delete(request $request)
+    {
         $ten_year_delete = SolidwasteTenYear::find($request->id);
         $ten_year_delete->delete();
-        return back()->with("message","10 Year Deleted");
+        return back()->with("message", "10 Year Deleted");
     }
 }
--- app/Http/Controllers/SolidwasteTenYearMonitoringController.php
+++ app/Http/Controllers/SolidwasteTenYearMonitoringController.php
@@ -9,63 +9,84 @@ use App\Models\SolidwasteTenYearMonitoring;
 
 class SolidwasteTenYearMonitoringController extends Controller
 {
-    public function create(request $request){
+    public function create(request $request)
+    {
         $id = $request->id;
         $lce_info = DB::table('tbl_solidwaste_lce as a')
-        ->select('a.*', 'b.provDesc', 'c.citymunDesc', 'd.brgyDesc', 'c.districtCode')
-        ->leftjoin('ref_province as b','a.lce_province_FK','=','b.PK_province_ID')
-        ->leftjoin('ref_citymun as c','a.lce_municipality_FK','=','c.PK_citymun_ID')
-        ->leftjoin('ref_brgy as d','a.lce_barangay_FK','=','d.PK_brgy_ID')
-        ->where('id',$id)
-        ->get();
-        return Inertia::render("pages/swm/Form/TenYearMonitoringForm",[
-            'lce_info'=>$lce_info,
+            ->select('a.*', 'b.provDesc', 'c.citymunDesc', 'd.brgyDesc', 'c.districtCode')
+            ->leftjoin('ref_province as b', 'a.lce_province_FK', '=', 'b.PK_province_ID')
+            ->leftjoin('ref_citymun as c', 'a.lce_municipality_FK', '=', 'c.PK_citymun_ID')
+            ->leftjoin('ref_brgy as d', 'a.lce_barangay_FK', '=', 'd.PK_brgy_ID')
+            ->where('id', $id)
+            ->get();
+        return Inertia::render("pages/swm/Form/TenYrMonitoringForm", [
+            'lce_info' => $lce_info,
         ]);
     }
 
-    public function ten_year_monitoring_edit(request $request){
+    public function ten_year_monitoring_edit(request $request)
+    {
         $id = $request->id;
         $ten_year_monitoring_edit = DB::table('tbl_solidwaste_ten_year_monitoring as a')
-        ->select('a.*', 'c.provDesc', 'd.citymunDesc', 'e.brgyDesc', 'd.districtCode')
-        ->leftjoin('tbl_solidwaste_ten_year as b','a.ten_year_FK','=','b.id')
-        ->leftjoin('tbl_solidwaste_lce as c','b.lce_FK','=','c.id')
-        ->leftjoin('ref_province as d','c.lce_province_FK','=','d.PK_province_ID')
-        ->leftjoin('ref_citymun as e','c.lce_municipality_FK','=','e.PK_citymun_ID')
-        ->leftjoin('ref_brgy as f','c.lce_barangay_FK','=','f.PK_brgy_ID')
-        ->where('a.id',$id)->get();
-        return Inertia::render("pages/swm/Form/TenYearMonitoringForm",[
-            'ten_year_monitoring_edit'=>$ten_year_monitoring_edit,
+            ->select('a.*', 'd.provDesc', 'e.citymunDesc', 'f.brgyDesc', 'e.districtCode')
+            ->leftjoin('tbl_solidwaste_ten_year as b', 'a.ten_year_FK', '=', 'b.id')
+            ->leftjoin('tbl_solidwaste_lce as c', 'b.lce_FK', '=', 'c.id')
+            ->leftjoin('ref_province as d', 'c.lce_province_FK', '=', 'd.PK_province_ID')
+            ->leftjoin('ref_citymun as e', 'c.lce_municipality_FK', '=', 'e.PK_citymun_ID')
+            ->leftjoin('ref_brgy as f', 'c.lce_barangay_FK', '=', 'f.PK_brgy_ID')
+            ->where('a.id', $id)->get();
+        return Inertia::render("pages/swm/Form/TenYrMonitoringForm", [
+            'ten_year_monitoring_edit' => $ten_year_monitoring_edit,
         ]);
     }
 
-    public function ten_year_monitoring_register_process(request $request){
+    public function ten_year_monitoring_register_process(request $request)
+    {
         $query = new SolidwasteTenYearMonitoring();
         $query->ten_year_mon_status = $request->ten_year_mon_status;
         $query->ten_year_mon_date_monitored = $request->ten_year_mon_date_monitored;
         $query->ten_year_mon_date_submitted = $request->ten_year_mon_date_submitted;
         $query->ten_year_mon_iis_number = $request->ten_year_mon_iis_number;
-        $query->ten_year_mon_copy_report = $request->ten_year_mon_copy_report;
+        $query->ten_year_mon_copy_report = !empty($request->ten_year_mon_copy_report) ? true : false;
         $query->ten_year_mon_by = $request->ten_year_mon_by;
-        $query->lce_FK = $request->lce_FK;
+        $query->ten_year_FK = $request->ten_year_FK;
         $query->save();
-        return back()->with("message","10 Year Monitoring Created");
+        if ($request->ten_year_mon_copy_report) {
+            foreach ($request->ten_year_mon_copy_report as $file) {
+                $query
+                    ->addMedia($file)
+                    ->preservingOriginal()
+                    ->toMediaCollection("copy_report", "copy_report");
+            }
+        }
+        return redirect()->route("ten_year_edit",["id"=>$request->ten_year_FK])->with("message", "10 Year Monitoring Created");
     }
 
-    public function ten_year_monitoring_update_process(request $request){
+    public function ten_year_monitoring_update_process(request $request)
+    {
         $query = SolidwasteTenYearMonitoring::find($request->id);
         $query->ten_year_mon_status = $request->ten_year_mon_status;
         $query->ten_year_mon_date_monitored = $request->ten_year_mon_date_monitored;
         $query->ten_year_mon_date_submitted = $request->ten_year_mon_date_submitted;
         $query->ten_year_mon_iis_number = $request->ten_year_mon_iis_number;
-        $query->ten_year_mon_copy_report = $request->ten_year_mon_copy_report;
+        // $query->ten_year_mon_copy_report = $request->ten_year_mon_copy_report;
         $query->ten_year_mon_by = $request->ten_year_mon_by;
-        $query->lce_FK = $request->lce_FK;
+        $query->ten_year_FK = $request->ten_year_FK;
         $query->save();
-        return back()->with("message","10 Year Monitoring Updated");
+        if ($request->ten_year_mon_copy_report) {
+            foreach ($request->ten_year_mon_copy_report as $file) {
+                $query
+                    ->addMedia($file)
+                    ->preservingOriginal()
+                    ->toMediaCollection("copy_report", "copy_report");
+            }
+        }
+        return redirect()->route("ten_year_edit",["id"=>$request->ten_year_FK])->with("message", "10 Year Monitoring Updated");
     }
-    public function ten_year_monitoring_delete(request $request){
+    public function ten_year_monitoring_delete(request $request)
+    {
         $ten_year_monitoring_delete = SolidwasteTenYearMonitoring::find($request->id);
         $ten_year_monitoring_delete->delete();
-        return back()->with("message","10 Year Monitoring Deleted");
+        return back()->with("message", "10 Year Monitoring Deleted");
     }
 }
