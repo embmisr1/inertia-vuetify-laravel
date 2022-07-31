1aeb8cac2f5ad401820a8f3e7047b9d56d3f8eae
russelowens <russelowens.miranda@lorma.edu>
2022-07-31 09:15:49 +0800

    07312022_0915

--- app/Http/Controllers/SolidwasteTenYearController.php
+++ app/Http/Controllers/SolidwasteTenYearController.php
@@ -13,11 +13,12 @@ class SolidwasteTenYearController extends Controller
 
     public function create(request $request){
         $id = $request->id;
-        $lce_info = DB::table('tbl_solidwaste_lce as a')
-        ->select('a.*', 'b.provDesc', 'c.citymunDesc', 'd.brgyDesc', 'c.districtCode')
-        ->leftjoin('ref_province as b','a.lce_province_FK','=','b.PK_province_ID')
-        ->leftjoin('ref_citymun as c','a.lce_municipality_FK','=','c.PK_citymun_ID')
-        ->leftjoin('ref_brgy as d','a.lce_barangay_FK','=','d.PK_brgy_ID')
+        $lce_info = DB::table('tbl_solidwaste_ten_year as a')
+        ->select('a.*', 'c.provDesc', 'd.citymunDesc', 'e.brgyDesc', 'd.districtCode')
+        ->leftjoin('tbl_solidwaste_lce as b','a.lce_FK','=','b.id')
+        ->leftjoin('ref_province as c','b.lce_province_FK','=','c.PK_province_ID')
+        ->leftjoin('ref_citymun as d','b.lce_municipality_FK','=','d.PK_citymun_ID')
+        ->leftjoin('ref_brgy as e','b.lce_barangay_FK','=','e.PK_brgy_ID')
         ->where('id',$id)
         ->get();
         return Inertia::render("pages/swm/Form/TenYearForm",[
