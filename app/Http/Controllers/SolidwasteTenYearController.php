b083b74979184459ac91a1e8539a0b6cbb38d328
russelowens <russelowens.miranda@lorma.edu>
2022-08-02 09:02:46 +0800

    08022022_0902

--- app/Http/Controllers/SolidwasteTenYearController.php
+++ app/Http/Controllers/SolidwasteTenYearController.php
@@ -8,6 +8,7 @@ use Inertia\Inertia;
 use Illuminate\Support\Facades\DB;
 use App\Models\SolidwasteTenYear;
 use App\Models\SolidwasteTenYearMonitoring;
+use App\Models\SolidwasteTenYearFindings;
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 class SolidwasteTenYearController extends Controller
@@ -32,12 +33,33 @@ class SolidwasteTenYearController extends Controller
     {
         $id = $request->id;
         $ten_year_edit = DB::table('tbl_solidwaste_ten_year as a')
-            ->select("a.id", "a.lce_FK", 'a.ten_year_planning_period', 'a.ten_year_year_approved', 'a.ten_year_number', 'c.provDesc', 'd.citymunDesc', 'e.brgyDesc', 'd.districtCode')
+            ->select(
+                "a.id", 
+                "a.lce_FK", 
+                "a.ten_year_planning_period", 
+                "a.ten_year_year_approved", 
+                "a.ten_year_number", 
+                "c.provDesc", 
+                "d.citymunDesc", 
+                "e.brgyDesc", 
+                "d.districtCode",
+                "f.finding_a",
+                "f.finding_b",
+                "f.finding_c",
+                "f.finding_d",
+                "f.finding_e",
+                "f.finding_f",
+                "f.finding_g",
+                "f.finding_h",
+                "f.finding_i",
+                "f.finding_j",
+            )
             // ->select('a.*', 'c.provDesc', 'd.citymunDesc', 'e.brgyDesc', 'd.districtCode')
             ->leftjoin('tbl_solidwaste_lce as b', 'a.lce_FK', '=', 'b.id')
             ->leftjoin('ref_province as c', 'b.lce_province_FK', '=', 'c.PK_province_ID')
             ->leftjoin('ref_citymun as d', 'b.lce_municipality_FK', '=', 'd.PK_citymun_ID')
             ->leftjoin('ref_brgy as e', 'b.lce_barangay_FK', '=', 'e.PK_brgy_ID')
+            ->leftjoin('tbl_solidwaste_ten_year_findings as f', 'f.ten_year_FK', '=', 'a.id')
             ->where('a.id', $id)->get();
         $ten_year_monitoring_list = SolidwasteTenYearMonitoring::where('ten_year_FK', $id)->get();
         $attachements = SolidwasteTenYear::where("id", $id)->get();
@@ -50,7 +72,6 @@ class SolidwasteTenYearController extends Controller
                 "copy_form" => AttachmentResource::collection($attachements[0]->getMedia("copy_form")),
                 "copy_resolution" => AttachmentResource::collection($attachements[0]->getMedia("copy_resolution")),
             ],
-            'ten_year_findings_array'=> json_decode($ten_year_edit[0]->ten_year_findings),
         ]);
     }
 
@@ -65,9 +86,25 @@ class SolidwasteTenYearController extends Controller
             $query->ten_year_copy_plan = !empty($request->ten_year_copy_plan) ? true : false;
             $query->ten_year_copy_resolution = !empty($request->ten_year_copy_resolution) ? true : false;
             $query->ten_year_copy_form = !empty($request->ten_year_copy_form) ? true : false;
-            $query->ten_year_findings = json_encode($request->ten_year_findings);
             $query->lce_FK = $request->lce_FK;
             $query->save();
+
+            if(isset($request->ten_year_findings)){
+                foreach($request->ten_year_findings as $findings){
+                    $query = new SolidwasteTenYearFindings();
+                    $query->finding_a = $findings[0];
+                    $query->finding_b = $findings[1];
+                    $query->finding_c = $findings[2];
+                    $query->finding_d = $findings[3];
+                    $query->finding_e = $findings[4];
+                    $query->finding_f = $findings[5];
+                    $query->finding_g = $findings[6];
+                    $query->finding_h = $findings[7];
+                    $query->finding_i = $findings[8];
+                    $query->finding_j = $findings[9];
+                    $query->save();
+                }
+            }
 
             if ($request->ten_year_file) {
                 foreach ($request->ten_year_file as $file) {
@@ -113,7 +150,7 @@ class SolidwasteTenYearController extends Controller
     public function ten_year_update_process(request $request)
     {
         try {
-            $query = SolidwasteTenYear::find($request->id);
+        $query = SolidwasteTenYear::find($request->id);
         $query->ten_year_planning_period = $request->ten_year_planning_period;
         $query->ten_year_year_approved = $request->ten_year_year_approved;
         $query->ten_year_number = $request->ten_year_number;
@@ -121,9 +158,28 @@ class SolidwasteTenYearController extends Controller
         // $query->ten_year_copy_plan = $request->ten_year_copy_plan;
         // $query->ten_year_copy_resolution = $request->ten_year_copy_resolution;
         // $query->ten_year_copy_form = $request->ten_year_copy_form;
-        $query->ten_year_findings = json_encode($request->ten_year_findings);
         $query->lce_FK = $request->lce_FK;
         $query->save();
+
+        if(isset($request->ten_year_findings)){
+            foreach($request->ten_year_findings as $findings){
+                $query = SolidwasteTenYearFindings::where('ten_year_FK',$request->id)
+                ->update([
+                    'finding_a'=>$findings[0],
+                    'finding_b'=>$findings[0],
+                    'finding_c'=>$findings[0],
+                    'finding_d'=>$findings[0],
+                    'finding_e'=>$findings[0],
+                    'finding_f'=>$findings[0],
+                    'finding_g'=>$findings[0],
+                    'finding_h'=>$findings[0],
+                    'finding_i'=>$findings[0],
+                    'finding_j'=>$findings[0],
+                ]);
+                $query->save();
+            }
+        }
+
         if ($request->ten_year_file) {
             foreach ($request->ten_year_file as $file) {
                 $query
@@ -186,6 +242,8 @@ class SolidwasteTenYearController extends Controller
     {
         $ten_year_delete = SolidwasteTenYear::find($request->id);
         $ten_year_delete->delete();
+        $ten_year_findings_delete = SolidwasteTenYearFindings::where('ten_year_FK',$request->id);
+        $ten_year_findings_delete->delete();
         return back()->with("message", "10 Year Deleted");
     }
 
