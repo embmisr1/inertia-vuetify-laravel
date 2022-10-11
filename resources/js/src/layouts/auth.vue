5caf38a519bc2a6a7e1265a7a12a9625c47efc52
allandaryldev <allandaryl.dev@gmail.com>
2022-10-12 07:40:17 +0800

    init

--- resources/js/src/layouts/auth.vue
+++ resources/js/src/layouts/auth.vue
@@ -1,31 +1,31 @@
 <template>
-    <!-- App.vue -->
+  <!-- App.vue -->
 
-    <v-app>
-        <!-- Sizes your content based upon application components -->
-        <v-main>
-            <!-- Provides the application the proper gutter -->
-            <v-container  class="min-h-screen grid place-content-center">
-                <!-- If using vue-router -->
-                <slot />
-            </v-container>
-        </v-main>
+  <v-app>
+    <!-- Sizes your content based upon application components -->
+    <v-main>
+      <!-- Provides the application the proper gutter -->
+      <div class="h-screen bg-gradient-to-t from-lime-400 via-lime-200 to-lime-400">
+        <!-- If using vue-router -->
+        <slot />
+      </div>
+    </v-main>
 
-        <v-footer app rounded >
-          <v-spacer></v-spacer>
-          &copy; DENR - EMB REGION 1 - UNISYS
-        </v-footer>
-    </v-app>
+    <!-- <v-footer app rounded class="bg-black">
+            <v-spacer></v-spacer>
+            &copy; DENR - EMB REGION 1 - UNISYS
+        </v-footer> -->
+  </v-app>
 </template>
 
 <script>
 export default {
-    metaInfo: {
-      // if no subcomponents specify a metaInfo.title, this title will be used
-      title: 'Unisys',
-      // all titles will be injected into this template
-      titleTemplate: '%s | EMB - Region 1 - Unisys'
-    },
+  metaInfo: {
+    // if no subcomponents specify a metaInfo.title, this title will be used
+    title: "Unisys",
+    // all titles will be injected into this template
+    titleTemplate: "%s | EMB - Region 1 - Unisys",
+  },
 };
 </script>
 
