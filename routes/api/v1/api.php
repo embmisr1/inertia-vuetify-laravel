c034c023c45f12025b85e3b35b2fccbc318bfbca
allan daryl <allandaryl.dev@gmail.com>
2022-12-13 11:06:13 +0800

    api v1 routes

--- routes/api/v1/api.php
+++ routes/api/v1/api.php
@@ -9,11 +9,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'api_login'])->name("api_login");
-// Route::group([
-//     "prefix"=>"movs"
-// ], function(){
-//     Route::get('/dashboard',DashboardController::class);
-// });
+
+
 
 Route::group([
     'middleware' => ['auth:sanctum'],
@@ -37,4 +34,8 @@ Route::group([
     Route::get('/attachment/{collection}/{model_id}',[AttachmentController::class,'get']);
     Route::delete('/attachment/{media}',[AttachmentController::class,'remove']);
     Route::post('/logout', [AuthController::class, 'api_logout'])->name("api_logout");
+
+    Route::get('/ftp',[AttachmentController::class,'downloadviaftp']);
+    Route::delete('/ftp/{media}',[AttachmentController::class,'removeviaftp']);
+    Route::get('/attachment/ftp',[AttachmentController::class,'get_ftp']);
 });
