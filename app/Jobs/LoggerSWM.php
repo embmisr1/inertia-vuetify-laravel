75d047ba905b4c79462a13c464f9f1d951ebd757
russelowens <russelowens.miranda@lorma.edu>
2023-01-03 10:03:05 +0800

    01032023_1002

--- app/Jobs/LoggerSWM.php
+++ app/Jobs/LoggerSWM.php
@@ -9,6 +9,7 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\DB;
 use App\Models\Logs;
 use App\Models\SolidwasteLCE;
 
@@ -46,14 +47,17 @@ class LoggerSWM implements ShouldQueue
     public function handle()
     {
         try {
-            $lce = SolidwasteLCE::select("lce_first_name")->where('id', $this->lce_FK)->get();
-
+            $lce = SolidwasteLCE::where('id', $this->lce_FK)->get();
+            $lce_address_province = DB::table('ref_province')->where('PK_province_ID', $lce[0]->lce_province_FK)->get();
+            $lce_address_municipality = DB::table('ref_citymun')->where('PK_citymun_ID', $lce[0]->lce_municipality_FK)->get();
+            $lce_address = $lce_address_province[0]->provDesc.', '.$lce_address_municipality[0]->citymunDesc;
             $new_log = new Logs();
 
             $new_log->model = $this->model;
             $new_log->model_id = $this->model_id;
             $new_log->user_id = $this->user_id;
-            $new_log->activity = $this->activity . "Mayor: " . $lce[0]->lce_first_name;
+            // $new_log->activity = $this->activity . "Mayor: " . $lce[0]->lce_first_name;
+            $new_log->activity = $this->activity . "LGU: " . $lce_address;
             $new_log->activity_type = $this->activity_type;
 
             $new_log->save();
