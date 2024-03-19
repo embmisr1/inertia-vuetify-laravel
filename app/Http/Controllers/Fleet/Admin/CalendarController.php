d2d0955b3feba0ed859114bb48a15bd2867938f0
allandaryl <allandaryl.dev@gmail.com>
2024-03-19 09:21:39 +0800

    dashboard calendar

--- app/Http/Controllers/Fleet/Admin/CalendarController.php
+++ app/Http/Controllers/Fleet/Admin/CalendarController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Fleet\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Fleet\RequestVehicleResource;
 use Illuminate\Http\Request;
 use App\Models\Fleet\Request as RequestModel;
 use Carbon\Carbon;
@@ -12,12 +13,22 @@ class CalendarController extends Controller
     public function __invoke()
     {
         try {
-            $current_month = RequestModel::with(['requested_user'])
-            ->whereBetween('created_at', [request('start'), request('end')])->get();
+            $isSameDate = request('start') === request('end');
+            $current_month = null;
+            if ($isSameDate) {
+                $current_month = RequestModel::with(['requested_user'])
+                    ->where('departure', 'like', '%' . request('start') . '%')
+                    ->get();
+            } else {
 
+                $current_month = RequestModel::with(['requested_user'])
+                    ->whereBetween('departure', [request('start'), request('end')])
+                    ->get();
+            }
             return response()->json(
                 [
-                    "data" => $current_month
+                    "data" => RequestVehicleResource::collection($current_month),
+                    // $isSameDate
                 ]
             );
         } catch (\Throwable $th) {
